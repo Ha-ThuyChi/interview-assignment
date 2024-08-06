dbea03038a53caa214149a07392c0969d35568fa
THuy Chi <chi.hathuy15@gmail.com>
2024-08-06 08:57:28 +0700

    cm video demo

--- src/App.js
+++ src/App.js
@@ -73,54 +73,58 @@ function App() {
 
   return (
     <div className='page'>
-      <div className='sort'>
-        Sort:  
-          <select value={property} onChange={(e) => setProperty(e.target.value)}>
-            <option value="">Property</option>
-            <option value="Username">Username</option>
-            <option value="Full name">Full name</option>
-          </select> 
-        by: 
-          <select value={order} onChange={(e) => setOrder(e.target.value)}>
-            <option value="">Order</option>
-            <option value="Ascending">Ascending</option>
-            <option value="Descending">Descending</option>
-          </select> 
-        <button onClick={e => handleSorting(e)}>Sort</button>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            {headers.map((header) => (
-              <th key={header}>{header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {viewedUsers.map((user) => (
-            <tr key={user.login.username}>
-              <td>{user.name.title} {user.name.first} {user.name.last}</td>
-              <td>{user.login.username}</td>
-              <td><img src={user.picture.thumbnail} alt="Thumbnail" /></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className='pagination'>
-        {pageNum > 1 && (
-          <button onClick={() => setPageNum(pageNum - 1)}>Previous</button>
-        )}
-        <button onClick={() => setPageNum(1)} className={pageNum === 1 ? 'active' : ''}>1</button> 
-        {pageNum > 1 && pageNum < 10 ? (
-          <button className='active'>{pageNum}</button>
-        ) : (
-          <button onClick={() => setPageNum(5)}>5</button>
-        )} 
-        <button onClick={() => setPageNum(10)} className={pageNum === 10 ? 'active' : ''}>10</button>
-        {pageNum < 10 && (
-          <button onClick={() => setPageNum(pageNum + 1)}>Next</button>
-        )}
-      </div>
+      {viewedUsers.length > 0 && (
+        <>
+          <div className='sort'>
+            Sort:  
+              <select value={property} onChange={(e) => setProperty(e.target.value)}>
+                <option value="">Property</option>
+                <option value="Username">Username</option>
+                <option value="Full name">Full name</option>
+              </select> 
+            by: 
+              <select value={order} onChange={(e) => setOrder(e.target.value)}>
+                <option value="">Order</option>
+                <option value="Ascending">Ascending</option>
+                <option value="Descending">Descending</option>
+              </select> 
+            <button onClick={e => handleSorting(e)}>Sort</button>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                {headers.map((header) => (
+                  <th key={header}>{header}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {viewedUsers.map((user) => (
+                <tr key={user.login.username}>
+                  <td>{user.name.title} {user.name.first} {user.name.last}</td>
+                  <td>{user.login.username}</td>
+                  <td><img src={user.picture.thumbnail} alt="Thumbnail" /></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className='pagination'>
+            {pageNum > 1 && (
+              <button onClick={() => setPageNum(pageNum - 1)}>Previous</button>
+            )}
+            <button onClick={() => setPageNum(1)} className={pageNum === 1 ? 'active' : ''}>1</button> 
+            {pageNum > 1 && pageNum < 10 ? (
+              <button className='active'>{pageNum}</button>
+            ) : (
+              <button onClick={() => setPageNum(5)}>5</button>
+            )} 
+            <button onClick={() => setPageNum(10)} className={pageNum === 10 ? 'active' : ''}>10</button>
+            {pageNum < 10 && (
+              <button onClick={() => setPageNum(pageNum + 1)}>Next</button>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
